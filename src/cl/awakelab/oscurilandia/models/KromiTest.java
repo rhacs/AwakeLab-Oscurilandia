05f1d405e48de2dc22123a2debcb9681c314ea35
Alumno <Alumno@DESKTOP-7VTS1QQ>
2020-02-24 17:15:49 -0300

    Agregue Clase Kromi Agregue testKromi

---
+++ src/cl/awakelab/oscurilandia/models/KromiTest.java
@@ -0,0 +1,71 @@
+package cl.awakelab.oscurilandia.models;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.Date;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import cl.awakelab.oscurilandia.helpers.RandomFecha;
+
+class KromiTest {
+
+     private Date fabricacion;
+     private String marca;
+     private Kromi micro;
+     private Ubicacion ubicacion;
+     private 
+     
+     @BeforeEach
+     void setup() throws Exception {
+        ubicacion = new Ubicacion(0,0);
+        marca = "Chevrolet";
+        fabricacion = new RandomFecha().getFecha();
+        micro = new Kromi(ubicacion);
+        micro.setMarca(marca);
+        micro.setFabricacion(fabricacion);
+     }
+     
+     
+     @Test
+     void shouldReturnfabricacion() {
+        assertEquals(fabricacion, micro.getFabricacion());
+     }
+     
+     @Test
+     void shouldChangeFabricacion() {
+        Date fecha = new RandomFecha().getFecha();
+        micro.setFabricacion(fecha);
+        assertEquals(fecha, micro.getFabricacion());
+     }
+     
+     @Test 
+     void shouldReturnMarca() {
+        assertEquals(marca, micro.getMarca());
+     }
+     
+     @Test
+     void shouldChangeMarca() {
+        String marca = "Chevrolet";
+        micro.setMarca(marca);
+        
+        assertEquals(marca, micro.getMarca());
+     }
+     
+     @Test
+     void shouldReturnUbicacion() {
+         assertEquals(ubicacion, micro.getUbicacion());
+     }
+     
+     @Test
+     void shouldChangeUbicacion() {
+         Ubicacion ubicacion = new Ubicacion(1,4);
+         micro.setUbicacion(ubicacion);
+        
+         assertEquals(ubicacion,micro.getUbicacion());
+     }
+     
+     
+     
+}
